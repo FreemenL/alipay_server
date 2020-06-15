c349a1b86fe19abc06f6c4239c3190462ff2be82
liuxiajiang <liuxiajiang@100tal.com>
2020-06-15 12:32:50 +0800

    feat:fixed tag

--- app/controller/home.js
+++ app/controller/home.js
@@ -9,10 +9,12 @@ class HomeController extends Controller {
   }
   async description(){
     const { ctx } = this;
+    console.log('ctx.request.body :>> ', ctx.request.body);
     ctx.body={
       code:0,
       request:ctx.request.body
     }
+    
   }
 }
 
